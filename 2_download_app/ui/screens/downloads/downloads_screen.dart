f2012f65c3ee265f84fa4acae3264f054f55bf4f
methy <methymuy@gmail.com>
2026-03-02 22:16:58 +0700

    Implement AppSettings repository and integrate theme state management

--- 2_download_app/ui/screens/downloads/downloads_screen.dart
+++ 2_download_app/ui/screens/downloads/downloads_screen.dart
@@ -1,66 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 import '../../providers/theme_color_provider.dart';
 import '../../theme/theme.dart';
 import 'widgets/download_controler.dart';
 import 'widgets/download_tile.dart';
 
 class DownloadsScreen extends StatelessWidget {
-  // Assume you have access to your global provider instance
-  final ThemeColorProvider themeProvider;
+  const DownloadsScreen({super.key});
 
-  // Create the list of fake ressources
-  final List<Ressource> ressources = [
+  // Fake resources
+  static final List<Ressource> ressources = [
     Ressource(name: "image1.png", size: 120),
-    Ressource(name: "image1.png", size: 500),
+    Ressource(name: "image2.png", size: 500),
     Ressource(name: "image3.png", size: 12000),
   ];
 
-  final List<DownloadController> controllers = [];
-
-  DownloadsScreen({super.key, required this.themeProvider}) {
-    // Create a controllers for each ressource
-    for (Ressource ressource in ressources) {
-      controllers.add(DownloadController(ressource));
-    }
-  }
+  // Controllers list
+  static final List<DownloadController> controllers =
+      ressources.map((r) => DownloadController(r)).toList();
 
   @override
   Widget build(BuildContext context) {
-    return ListenableBuilder(
-      listenable: themeProvider,
-      builder: (context, child) {
-        final theme = themeProvider.currentThemeColor;
+    final themeProvider = context.watch<ThemeColorProvider>();
+    final theme = themeProvider.currentThemeColor;
 
-        return Container(
-          color: theme.backgroundColor,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(height: 16),
-              Text(
-                "Downloads",
-                style: AppTextStyles.heading.copyWith(
-                  color: theme.color,
-                ),
-              ),
+    return Container(
+      color: theme.backgroundColor,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(height: 16),
 
-              SizedBox(height: 50),
-
-              // TODO - Add the Download tiles
-              Expanded(
-                child: ListView.builder(
-                  itemCount: controllers.length,
-                  itemBuilder: (context, index) {
-                    return DownloadTile(
-                      controller: controllers[index],
-                    );
-                  },
-                ),
-              ),
-            ],
+          Text(
+            "Downloads",
+            style: AppTextStyles.heading.copyWith(
+              color: theme.color,
+            ),
           ),
-        );
-      },
+
+          const SizedBox(height: 50),
+
+          Expanded(
+            child: ListView.builder(
+              itemCount: controllers.length,
+              itemBuilder: (context, index) {
+                return DownloadTile(
+                  controller: controllers[index],
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
